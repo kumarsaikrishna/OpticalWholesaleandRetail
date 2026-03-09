8060fd0d9f270e93343d2be54eb78302d073df0c
sai <saikrishnameesala6300@gmail.com>
2026-03-09 23:07:02 +0530

    changes

--- Utilities/GenericResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpticalFibersRetailShop.Utilities
-{
-    public class GenericResponse
-    {
-        public string message { get; set; }
-        public int statuCode { get; set; }
-        public int currentId { get; set; }
-    }
-}
